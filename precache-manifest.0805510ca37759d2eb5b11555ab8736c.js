f93c7756207729213d7d83c44bcdad4432c39fd0
AshishAkGit <ashish.khadilkar@gmail.com>
2020-09-01 10:24:13 +0530

    Updates

---
+++ precache-manifest.0805510ca37759d2eb5b11555ab8736c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bae3f9ead527931466f1832464664a",
+    "url": "/my-engg-group/index.html"
+  },
+  {
+    "revision": "2abfb5b82fb139516697",
+    "url": "/my-engg-group/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd25de034dcf79bbc336",
+    "url": "/my-engg-group/static/js/2.91b9a64c.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/my-engg-group/static/js/2.91b9a64c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abfb5b82fb139516697",
+    "url": "/my-engg-group/static/js/main.abd9dc6a.chunk.js"
+  },
+  {
+    "revision": "4450a48e99c527e96698",
+    "url": "/my-engg-group/static/js/runtime-main.b91df669.js"
+  }
+]);
